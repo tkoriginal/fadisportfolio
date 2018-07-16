ec19bbcbcad08f6ba4b98368c96e25d3c063ef5d
Tauqueer Khan <tauqueerkhan@Tauqueers-MacBook-Air.local>
2018-07-15 21:47:07 -0400

    UPdated

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('//views/home'))
+  .get('/', (req, res) => res.render('views/home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
