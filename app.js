7804bfb7f094d237ba5f548baf57e69644d44b8c
Tauqueer Khan <tauqueerkhan@Tauqueers-MacBook-Air.local>
2018-07-15 21:16:21 -0400

    updated

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.set("view-engine","ejs");
+app.engine('html',require(ejs).renderFile);
+app.set("view-engine", "ejs");
 
 app.get("/", function(req,res){
     res.render("home");
